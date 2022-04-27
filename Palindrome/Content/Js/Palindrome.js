75f7bb0c5b412cf22b9523e81208b80bcb9b3649
mhmdr <mhmdr@DESKTOP-AFMALD3>
2022-04-27 02:28:07 +0200

    Add project files.

---
+++ Palindrome/Content/Js/Palindrome.js
@@ -0,0 +1,98 @@
+﻿
+// JavaScript Palidrome Test
+
+var JsTextBox = document.getElementById("JsText");
+
+
+JsTextBox.onkeyup = function () {
+
+    let textEntered = JsTextBox.value;
+
+    if (textEntered == "") {
+        JsTextBox.style.backgroundColor = "White";
+    } else if (checkPalindrom(textEntered) == true) {
+        JsTextBox.style.backgroundColor = "#36a143";
+    }
+    else {
+        JsTextBox.style.backgroundColor = "Red";
+    }
+
+};
+
+function checkPalindrom(textEntered) {
+    return textEntered == textEntered.split('').reverse().join('');
+}
+
+// Sql Palidrome Test
+
+var SqlTextBox = document.getElementById("SqlText");
+var SqlBtn = document.getElementById("SqlBtn");
+
+SqlBtn.onclick = function () {
+
+    let textEntered = SqlTextBox.value;
+    if (textEntered == "") {
+        SqlTextBox.style.backgroundColor = "White";
+        alert("Please enter a word ");
+        return;
+    }
+
+    $.ajax({
+        type: "Post",
+        dataType: "json",
+        url: "Home/CheckPalindromeSqlServer",
+        data: { word: textEntered },
+        success: function (result) {
+
+            if (result == true || result == "true") {
+              
+                SqlTextBox.style.backgroundColor = "#36a143";
+            }
+            else {
+                SqlTextBox.style.backgroundColor = "Red";
+            }
+        },
+        error: function (data) {
+            console.log(data);
+        }
+
+    });
+
+}
+
+
+// Csharp Palidrome Test
+
+var CsharpTextBox = document.getElementById("CsharpText");
+var CsharpBtn = document.getElementById("CsharpBtn");
+
+CsharpBtn.onclick = function () {
+
+    let textEntered = CsharpTextBox.value;
+    if (textEntered == "") {
+        CsharpTextBox.style.backgroundColor = "White";
+        alert("Please enter a word ");
+        return;
+    }
+
+    $.ajax({
+        type: "Post",
+        dataType: "json",
+        url: "Home/CheckPalindrome",
+        data: { word: textEntered },
+        success: function (result) {
+
+            if (result == true) {
+                CsharpTextBox.style.backgroundColor = "#36a143";
+            }
+            else {
+                CsharpTextBox.style.backgroundColor = "Red";
+            }
+        },
+        error: function (data) {
+            console.log(data);
+        }
+
+    });
+
+}
---
+++ Palindrome/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Palindrome.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Palindrome.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public JsonResult CheckPalindrome(string word)
+        {
+            bool result = word.SequenceEqual(word.Reverse());
+            return Json(result);
+        }  
+
+        public JsonResult CheckPalindromeSqlServer(string word)
+        {
+            using (PalindromeEntities palindromeEntities = new PalindromeEntities())
+            {
+                var check = palindromeEntities.Database.SqlQuery<string>("SELECT dbo.CheckPalindrome('"+ word + "')");
+                string result = "";
+                foreach(var item in check)
+                {
+                    result += item;
+                }
+                return Json(result);
+            }  
+        }
+    }
+
+
+
+
+}
